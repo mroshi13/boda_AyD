0016415b9cea805cfc0072737a6f96d29094d298
Alec Ortega <aalecortega@gmail.com>
2026-07-10 13:41:34 -0700

    Changed font-sizes

--- app/page.tsx
+++ app/page.tsx
@@ -276,7 +276,7 @@ export default function HomePage() {
                     Lo que necesitas
                   </span>
 
-                  <span className={` ${tang.className} text-[clamp(3rem,9vw,7rem)]`}>
+                  <span className={` ${tang.className} text-[clamp(6rem,9vw,7rem)]`}>
                     saber
                   </span>
                 </h1>
@@ -345,7 +345,7 @@ export default function HomePage() {
                     <div className="flex items-center gap-4">
                       <h2 className="flex flex-col text-center font-bold leading-[0.9] md:text-left">
                         <span className={`${antic.className} text-[clamp(1.5rem,9vw,3rem)]`}>Código de</span>
-                        <span className={`${tang.className} text-[clamp(1.5rem,9vw,3.5rem)]`}>Vestimenta</span>
+                        <span className={`${tang.className} text-[clamp(4.5rem,9vw,3.5rem)]`}>Vestimenta</span>
                       </h2>
 
                       <img
@@ -423,7 +423,7 @@ export default function HomePage() {
                 <h1
                   className={`flex flex-col items-center text-center font-semibold leading-[0.9]`}
                 >
-                  <span className={` ${antic.className} text-[clamp(2.8rem,7vw,2rem)]`}>
+                  <span className={` ${antic.className} text-[clamp(1.5rem,7vw,2rem)]`}>
                     Opciones de
                   </span>
 
